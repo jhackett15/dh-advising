12b9abc9ac6bfbbd6820e4d47e7da1d26878313d
jhackett15 <hackett.justin15@gmail.com>
2021-09-30 21:58:10 -0700

    Update nav.js

--- public/components/nav.js
+++ public/components/nav.js
@@ -5,32 +5,32 @@ import Image from 'next/image'
 function Nav() {
   return (
     
-      <div className="max-w-md font-sans mx-auto max-w-4xl mt-20">
+      <div className="max-w-md font-sans mx-auto max-w-4xl py-30"">
       <nav className="border-b-2">
         <div className="w-full lg:w-auto">
-          <div className="text-lg lg:flex-1 flex justify-center mb-6">
+          <div className="text-lg lg:flex-1 flex justify-center pb-8">
           <Link href="/">
-            <a className="m-6 lg:inline-block lg:mt-0">
+            <a className="m-6 lg:inline-block">
               HOME
             </a>
             </Link>
             <Link href="/about">
-            <a className="m-6 lg:inline-block lg:mt-0">
+            <a className="m-6 lg:inline-block">
               ABOUT
             </a>
             </Link>
             <Link href="/advising">
-            <a className="m-6 lg:inline-block lg:mt-0">
+            <a className="m-6 lg:inline-block">
               ADVISING
             </a>
             </Link>
             <Link href="/faq">
-            <a className="m-6 lg:inline-block lg:mt-0">
+            <a className="m-6 lg:inline-block">
               FAQ
             </a>
             </Link>
             <Link href="/blog">
-              <a className="m-6 lg:inline-block lg:mt-0">
+              <a className="m-6 lg:inline-block">
                 BLOG
               </a>
           </Link>
