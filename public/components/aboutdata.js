2e75fbefcc545144bc9ca6fee5ce2165480e1fcc
jhackett15 <hackett.justin15@gmail.com>
2021-09-30 21:33:56 -0700

    Delete aboutdata.js

--- public/components/aboutdata.js
+++
@@ -1,8 +0,0 @@
-
-
-export const questionsAndAnswers =[{
-    id:1, question:`How would you describe tour leadership style?`, answer:`I am a collaborato who focuses on a team apporach to tackle the underlying issues. My style stems from my desire to teach and help others tackle issues. I encourage people to take on additional responsibilites while supporting them, enabling them to grow and succeed personally & professionally.`, 
-    id:2, question:`How do you get people to perform at high levels?`, answer: `I provide the opportunity for business owners to work with their team to take on additional accountability and prove themselves. People perform at superior levels when leadership creates a vision and encourages them to work toward it. i allow for failures along the way as it builds trust and energy to achieve the vision.`,
-    id:3, question:`How do you turn a challenge into an opportunity?`, answer:`All challenges are opportunities, just as a customer saying 'no' really means'not yet'. Challenges often suffer from poor definition; the issue on the surface is rarely the real problem. Challenges become opportunities through a colloborative effor to better define the problem and create the solution in line with the vision of the organization.`, 
-    id:4, question:`What distinct advantage do you bring to your clients?`, answer:`My competitive advantage is my leadership, finance & strategy, consulting and executive experience. the vastness of my experience combined with a colloborative apporach forms the foundation for growth and operational excellence. A deep understanding of professional management and the proven ability to create a solid manangement team promotes a sustainable organtixation and increased value.`
-  }];
