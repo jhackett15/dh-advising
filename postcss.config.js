15549be856222b565b295ebf78b7f841b27f45cb
jhackett15 <hackett.justin15@gmail.com>
2021-10-03 11:44:52 -0700

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,6 @@
-
-
 module.exports = {
-   plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-      ...(process.env.NODE_ENV === 'production'
-         ? {
-              '@fullhuman/postcss-purgecss': {
-                 // added sections folder and changed extension to jsx
-                 content: ['./src/components/**/*.jsx', './src/pages/**/*.js'],
-                 defaultExtractor: content =>
-                    content.match(/[\w-/:]+(?<!:)/g) || [],
-              },
-           }
-         : {}),
-   },
-}`
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
